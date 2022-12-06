8128205ee3676c33489aa1f99815a48237af2722
ojasuno <ojasuno21@gmail.com>
2022-12-06 17:29:04 +1100

    theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,14 +4,14 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/ojasuno/pigeons-docs',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'discord.gg/josie',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/ojasuno/pigeons-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: "[Unofficial Don't Feed The Pigeosn]",
   },
 }
 
