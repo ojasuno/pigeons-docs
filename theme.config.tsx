83fefd4c161e9917fdf3a4bf05756eee4fe2fa93
ojasuno <ojasuno21@gmail.com>
2022-12-09 11:02:54 +1100

    theme config

--- theme.config.tsx
+++ theme.config.tsx
@@ -36,6 +36,7 @@ const config: DocsThemeConfig = {
       <meta property="og:url" content={`https://pigeons.monster${asPath}`} />
       <meta property="og:title" content={frontMatter.title || "Pigeons"} />
       <meta property="og:description" content={frontMatter.description || "Unoffical Don't Feed The Pigeons"} />
+      <meta property="og:image" content="https://pigeons.monster/_assets/_social/12-pigeons.jpgeg" />
     </>
   },
   faviconGlyph: '🖖',
